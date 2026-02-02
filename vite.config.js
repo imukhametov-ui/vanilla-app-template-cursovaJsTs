b72bcdbec5e6dd28f88bf9b77ee0a26264dcb810
IHOR <i.mukhametov@student.bachelor-goit.com.ua>
2026-02-02 18:10:54 +0200

    Fix vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,22 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    base: '/vanilla-app-template-cursovaJsTs/',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig({
+  root: 'src',
+  base: '/vanilla-app-template-cursovaJsTs/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        favorites: './src/favorites.html',
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-    ],
-  };
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+  ],
 });
