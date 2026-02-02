728b36bbb9e3d21bc2595ef7e48ea08b43339356
IHOR <i.mukhametov@student.bachelor-goit.com.ua>
2026-02-02 18:02:27 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/vanilla-app-template-cursovaJsTs/',
     build: {
       sourcemap: true,
       rollupOptions: {
